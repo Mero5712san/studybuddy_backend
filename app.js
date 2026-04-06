03c7ef76b26e74314fa3c61068f8416c9652675e
Merosan <mrsn5712@gmail.com>
2026-04-06 23:05:11 +0530

    deploy

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return cors(corsOptions)(req, res, () => res.sendStatus(204));
+    }
+
+    return next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
