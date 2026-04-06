e13f3cab4142b77d308c8399ad5aa98bde1ac678
Merosan <mrsn5712@gmail.com>
2026-04-06 22:57:32 +0530

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000',
+    'http://localhost:5173'
+].filter(Boolean);
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const { Server } = require('socket.io');
 const app = require('./app');
 const db = require('./models');
 const server = http.createServer(app);
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000',
+    'http://localhost:5173'
+].filter(Boolean);
+
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
-        methods: ["GET", "POST"]
+        origin: allowedOrigins,
+        methods: ['GET', 'POST'],
+        credentials: true,
     }
 });
 
@@ -15,6 +22,6 @@ const io = new Server(server, {
 require('./sockets/socketHandler')(io);
 
 // db.sequelize.sync({ alter: true }).then(() => {
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // });
